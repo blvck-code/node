e52cb9c9a4c9ebfac71368f761d0c2e9db6a377a
Maurice Oluoch <53207394+blvck-code@users.noreply.github.com>
2024-03-21 23:30:10 +0300

    added middlewares

--- app.js
+++ app.js
@@ -1,21 +1,61 @@
-const express = require('express')
-
-
+const express = require("express");
 
 // express app
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+
+// app.set('views', 'myviews')
 
 // listen to request
-app.listen(8000)
+app.listen(8000);
 
-app.get('/', (req, res) => {
-    // res.send('<p>Home page</p>')
-    res.sendFile('./views/index.html', { root:  __dirname })
-})
+app.use((req, res, next) => {
+  console.log("new request made: ");
+  console.log("host: ", req.hostname);
+  console.log("path: ", req.path);
+  console.log("method: ", req.method);
+  next();
+});
 
-app.get('/about', (req, res) => {
-    // res.send('<p>About us page</p>')
-    res.sendFile('./views/about.html', { root:  __dirname })
-})
+app.get("/", (req, res) => {
+  // res.send('<p>Home page</p>')
+  //   res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
+});
 
+app.get("/about", (req, res) => {
+  // res.send('<p>About us page</p>')
+  //   res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
+});
+
+app.get("/blog/create", (req, res) => {
+  res.render("");
+});
+
+// redirect
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// 404 page, it has to be at the bottom most
+app.use((req, res) => {
+  // res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
+});
